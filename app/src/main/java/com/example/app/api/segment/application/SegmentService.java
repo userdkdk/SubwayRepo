7d80ea3c761225f5317602d5c8e4bef9dd28a9ca
dkdk <konzo321@naver.com>
2026-01-28 13:26:18 +0900

    FIX: Update segment attribute

--- app/src/main/java/com/example/app/api/segment/application/SegmentService.java
+++ app/src/main/java/com/example/app/api/segment/application/SegmentService.java
@@ -2,7 +2,6 @@ package com.example.app.api.segment.application;
 
 import com.example.app.api.segment.api.dto.request.UpdateSegmentRequest;
 import com.example.core.business.segment.SegmentRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
