83f9f1781edf1b88d9df32ea3dd91908e58b938a
dkdk <konzo321@naver.com>
2026-02-04 14:15:40 +0900

    FEAT: grafana 실행확인 - log format 수정필요

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,53 +9,50 @@ cd "$SERVER_DIR"
 
 DOCKER_APP_NAME="subway-repo"
 
-
 echo "PWD=$(pwd)"
 echo "Using env file: $ENV_FILE"
 
-# check db
-EXIST_DB=$(docker-compose -p subway-db -f docker/docker-compose.db.yml ps | grep -E "Up|running")
-if [ -z "$EXIST_DB" ]; then
+# check and run db
+if [ -z "$(docker compose -p subway-db ps -q)" ]; then
   echo "db 배포 시작 : $(date '+%Y-%m-%d %H:%M:%S')"
-  docker-compose -p subway-db -f docker/docker-compose.db.yml --env-file "$ENV_FILE" up -d
+  docker compose -p subway-db -f docker/docker-compose.db.yml --env-file "$ENV_FILE" up -d
+fi
+
+# check and run grafana
+if [ -z "$(docker compose -p grafana-container ps -q)" ]; then
+  echo "grafana 실행 시작 : $(date '+%Y-%m-%d %H:%M:%S')"
+  docker compose -p grafana-container -f docker/docker-compose.grafana.yml up -d
 fi
 
 
 # --- 배포 실행 ---
 # CHECK BLUE
-EXIST_BLUE=$(docker-compose -p "${DOCKER_APP_NAME}-blue" -f docker/docker-compose.blue.yml ps | grep -E "Up|running")
-
-if [ -z "$EXIST_BLUE" ]; then
+if [ -z "$(docker compose -p "$DOCKER_APP_NAME"-blue ps -q)" ]; then
   echo "blue 배포 시작 : $(date '+%Y-%m-%d %H:%M:%S')"
 
-  docker-compose -p "${DOCKER_APP_NAME}-blue" \
+  docker compose -p "${DOCKER_APP_NAME}-blue" \
     -f docker/docker-compose.blue.yml \
     --env-file "$ENV_FILE" \
     up -d --build
   sleep 20
-  NEW_CONTAINER_STATUS=$(docker-compose -p "${DOCKER_APP_NAME}-blue" -f docker/docker-compose.blue.yml ps | grep -E "Up|running")
-  if [ -n "$NEW_CONTAINER_STATUS" ]; then
+  if [ -n "$(docker compose -p "$DOCKER_APP_NAME"-green ps -q)" ]; then
     echo "blue 컨테이너 실행 확인. green 중단 시작"
-    docker-compose -p "${DOCKER_APP_NAME}-green" \
-        -f docker/docker-compose.green.yml \
-        --env-file "$ENV_FILE" \
-        down
+    docker compose -p "${DOCKER_APP_NAME}-green" down
+    echo "green 컨테이너 종료 완료"
   fi
   echo "blue 배포 완료 : $(date '+%Y-%m-%d %H:%M:%S')"
 else
   echo "green 배포 시작 : $(date '+%Y-%m-%d %H:%M:%S')"
 
-  docker-compose -p "${DOCKER_APP_NAME}-green" \
+  docker compose -p "${DOCKER_APP_NAME}-green" \
     -f docker/docker-compose.green.yml \
     --env-file "$ENV_FILE" \
     up -d --build
   sleep 20
-  NEW_CONTAINER_STATUS=$(docker-compose -p "${DOCKER_APP_NAME}-green" -f docker/docker-compose.green.yml ps | grep -E "Up|running")
-  if [ -n "$NEW_CONTAINER_STATUS" ]; then
-    docker-compose -p "${DOCKER_APP_NAME}-blue" \
-        -f docker/docker-compose.blue.yml \
-        --env-file "$ENV_FILE" \
-        down
+  if [ -n "$(docker compose -p "$DOCKER_APP_NAME"-blue ps -q)" ]; then
+    echo "green 컨테이너 실행 확인. blue 중단 시작"
+    docker compose -p "${DOCKER_APP_NAME}-blue" down
+    echo "blue 컨테이너 종료 완료"
   fi
   echo "green 배포 완료 : $(date '+%Y-%m-%d %H:%M:%S')"
 fi
---
+++ deploy/stopAll.sh
@@ -0,0 +1,4 @@
+docker compose -p subway-repo-blue down
+docker compose -p subway-repo-green down
+docker compose -p subway-db down
+docker compose -p grafana-container down
